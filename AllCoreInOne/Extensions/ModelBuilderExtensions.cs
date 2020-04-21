1511f20898d23a4e105f59105c91df580bbaf32d
Malik <malik@xdsol.com>
2020-04-21 22:56:11 +0500

    first commit

---
+++ AllCoreInOne/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using AllCoreInOne.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace AllCoreInOne.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().HasData(
+                new List<Department>()
+                {
+                    new Department{Id = Guid.NewGuid(),Name="Computer Science"},
+                    new Department{Id = Guid.NewGuid(),Name="Business Administration"},
+                    new Department{Id = Guid.NewGuid(),Name="Physics"}
+                }
+                );
+        }
+    }
+}
